68dc92851e7a4b2f08ed015bd5403b571b8451eb
Siim Kirjanen <siim.kirjanen@pipedrive.com>
2025-02-19 13:31:47 +0200

    Added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/Home/HomePage";
+import UserPage from "./pages/User/UserPage";
+
 function App() {
   return (
-    <div className="flex justify-center">
-      <button className="btn">Default</button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="user/:id" element={<UserPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
